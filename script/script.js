b2328d1f9670a50b2eea8d959065e2ba36c2f765
Michael2M <darak.ltd@yandex.ru>
2021-03-16 15:31:05 +0800

    feat:add active like

--- script/script.js
+++ script/script.js
@@ -73,16 +73,37 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 
 //это мой варинат изначальный. 
-let elementsCard = document.querySelector('.elements');//получили элемент родительского блока по идентификатору класса через метод querySelector и сохранили его в переменной 
+// let elementsCard = document.querySelector('.elements');//получили элемент родительского блока по идентификатору класса через метод querySelector и сохранили его в переменной 
 // console.log(elementCards);//проверили вывод переменной в конслоь - все найденные элементы выводятся
 
-let likeBtns = elementsCard.querySelectorAll('#like-btn');//получаю элемент из уже существующей переменной по id класса и сохранил в переменной - к этой переменной буду добавлять новый класс, где будет менятся изображение на черный фон.
-console.log(likeBtns);//проверил вывод, все выходит в консоль.
+let likeBtns = document.querySelectorAll('#like-btn');//получаю элементы из уже существующей переменной по id класса и сохранил в переменной - к этой переменной буду добавлять новый класс, где будет менятся изображение на черный фон.
+// console.log(likeBtns);//проверил вывод, все выходит в консоль.
 
-function linkButton () {// функция должна добавлять новый класс element__like_active к переменной likeBtns в которой хранятся все элементы.
-  likeBtns.classList.toggle('.element__like_active'); //берем переменную и к ее классам добавляем новый класс - и вот здесь затык!!!
+function clickButton () {
+    likeBtns.classList.toggle('element__like_active');
 }
-console.log(linkButton());
+
+ 
+// function clickButton () {
+//   for (i = 0; i < likeBtns.length; i++) {
+//     likeBtns[i].classList.add("element__like_active");
+//   }
+// }
+
+likeBtns.addEventListener('click', clickButton);
+console.log(likeBtns);
+
+// function linkButton () {// функция должна добавлять новый класс element__like_active к переменной likeBtns в которой хранятся все элементы.
+//   this.classList.add("element__like_active"); //берем переменную и к ее классам добавляем новый класс - и вот здесь затык!!!
+// }
+// console.log(linkButton());
+
+
+// for (var i = 0; i < elements.length; i++) {
+//     elements[i].classList.add("test2");
+// }
+
+
 // function clickHandler(){
 //   likeBtns.classList.add('element__like_active');
 //   // console.log(likeBtns.classList.contains('element__likr_active'));
@@ -91,3 +112,33 @@ console.log(linkButton());
 // likeBtns.addEventListener('click', clickHandler);
 
 // console.log(elmLikeBtns);
+
+
+document.querySelectorAll('#like-btn').forEach(function(e) {
+  e.addEventListener('click', function() {
+    this.classList.add('.element__like_active');
+  })
+}); 
+
+//-это эксперемент со сменой цвета всего бэкграунда (взял из интернета), когда срабатывает то окрашивает весь бэкграунд, и к тому же нет обратной смены фона.
+
+
+// function a() {
+//   likeBtns.setAttribute('element__like_active');
+//   console.log(likeBtns.setAttribute('element__like_active'));
+// }
+
+
+
+// созадим функцию которая будте проверять наличие артибута или его отсутсвие и добавлять или убавлять класс
+
+// function addClass() {
+//   if (likeBtns.hasAttribute('element__like_active') === true){
+//     likeBtns.removeAttribute('element__like_active')
+//     console.log(likeBtns);
+//   } else{
+//     likeBtns.addAttribute('element__like_active')
+//   }
+// }
+
+// likeBtns.addEventListener('click', addClass);
