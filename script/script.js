507cad8e6c6031e8fbd4c148e132bd3b58ff8a24
Michael2M <darak.ltd@yandex.ru>
2021-03-19 16:35:00 +0800

    fix:fixed mistakes after first review

--- script/script.js
+++ script/script.js
@@ -1,55 +1,45 @@
-//по клику на значек редактирования профиля открываем попап
-//по клику на крестик закрываем попап
-//по клику на кнопку сохранить закрываем попап
+/*
+Project Mesto-Russia
+Version 0.02a - 19.03.2021
 
+Description: Скрипт отвечает за форму заполнения данных пользователя на сайте
+По клику на иконке редактирования, открывается попап-форма, где доступны к заполнению поля Имени пользователя и его профессии
+Нажатие на кнопку Сохранить, перезаписывает введеные значения в карточку пользователя на сайте
 
-let popup = document.querySelector('.popup');
-
-let openPopupBtn = document.querySelector('.profile__edit-btn');
-
-let closePopupBtn = document.querySelector('.form__close-btn')
-
-function openPopup() {
-  popup.classList.add('popup_visible');
-};
-
-function closePopup() {
-  popup.classList.remove('popup_visible');
-};
-
-openPopupBtn.addEventListener('click', function(){
-  openPopup();
-});
-
-closePopupBtn.addEventListener('click', function(){
-  closePopup();
-});
-
-
+Michael2M (c) 2021
+email: darak.ltd@yandex.ru
+*/
 
 // при вводе в сроке имени, меняем имя вывода на странице
 // при вовде текста в строке должности, менем текст на странице 
 
 let formElement = document.querySelector('.form');
-
 let nameInput = document.querySelector('.profile__user-name');
-
 let jobInput = document.querySelector('.profile__user-job');
+let currentUserName = document.getElementById('user_name'); //получаем и записываем значение переменной из поля по id user-name
+let currentUserJob = document.getElementById('user_job'); //получаем и записываем значение переменной из поля по id user-job
+let popup = document.querySelector('.page__popup');
+let openPopupBtn = document.querySelector('.profile__button-edit');
+let closePopupBtn = document.querySelector('.popup__button-close');
 
 
 function formSubmitHandler(evt){
   evt.preventDefault();
-  
-  // Получите значение полей jobInput и nameInput из свойства value и назанчаем новые переменные для хранения новых значений введеных в форму
-  let currentUserName = document.getElementById('user-name'); //получаем и записываем занчение переменной из поля по id user-name
-  // console.log(currentUserName);
-  let currentUserJob = document.getElementById('user-job'); //получаем и записываем занчение переменной из поля по id user-job
-  // console.log(currentUserJob);
-  // Вставьте новые значения с помощью textContent
   nameInput.textContent = currentUserName.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
   jobInput.textContent = currentUserJob.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
-
   closePopup(); //используем уже готовую функцию для закрытия попапа
 }
 
+function openPopup() {
+  popup.classList.add('page__popup_visible');
+  currentUserName.value = nameInput.textContent;
+  currentUserJob.value = jobInput.textContent;
+};
+
+function closePopup() {
+  popup.classList.remove('page__popup_visible'); 
+};
+
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
