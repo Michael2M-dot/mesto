fbbd74581b75915fe757a0d0a780a3299c86cd02
Michael2M <darak.ltd@yandex.ru>
2021-03-26 09:48:25 +0800

    feat:added add elements to places

--- script/script.js
+++ script/script.js
@@ -13,59 +13,20 @@ email: darak.ltd@yandex.ru
 // при вводе в сроке имени, меняем имя вывода на странице
 // при вовде текста в строке должности, менем текст на странице 
 
-const formElement = document.querySelector('.form');
+const formUser = document.querySelector('#user-profile');
 const nameInput = document.querySelector('.profile__user-name');
 const jobInput = document.querySelector('.profile__user-job');
-const currentUserName = document.getElementById('user_name'); //получаем и записываем значение переменной из поля по id user-name
-const currentUserJob = document.getElementById('user_job'); //получаем и записываем значение переменной из поля по id user-job
+const currentUserName = document.querySelector('#user-name'); //получаем и записываем значение переменной из поля по id user-name
+const currentUserJob = document.querySelector('#user-job'); //получаем и записываем значение переменной из поля по id user-job
 const popupUser = document.querySelector('#edit-profile');
 const popupPlace = document.querySelector('#add-place');
-const openUserPopupBtn = document.querySelector('.profile__button-edit');
+const openUserPopupBtn = document.querySelector('#profile-editBtn');
 const closeUserPopupBtn = document.querySelector('#close-userPopup');
 const openPlacePopupBtn =document.querySelector('.profile__button-add');
 const closePlacePopupBtn = document.querySelector('#close-placePopup');
-
-
-function formSubmitHandler(evt){
-  evt.preventDefault();
-  nameInput.textContent = currentUserName.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
-  jobInput.textContent = currentUserJob.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
-  closePopup(); //используем уже готовую функцию для закрытия попапа
-} //функция кнопки Сохранить.
-
-function openUserPopup() {
-  popupUser.classList.add('page__popup_visible');
-  currentUserName.value = nameInput.textContent;
-  currentUserJob.value = jobInput.textContent;
-}; //функция открытия попапа
-
-function closeUserPopup() {
-  popupUser.classList.remove('page__popup_visible'); 
-};
-
-function openPlacePopup() {
-  popupPlace.classList.add('page__popup_visible');
-}
-
-function closePlacePopup() {
-  popupPlace.classList.remove('page__popup_visible');
-}
-
-
-openUserPopupBtn.addEventListener('click', openUserPopup);//слушатель для открытия попапа для редактирования профиля пользователя
-closeUserPopupBtn.addEventListener('click', closeUserPopup);//слушатель для закрытия попапа
-openPlacePopupBtn.addEventListener('click', openPlacePopup);
-closePlacePopupBtn.addEventListener('click', closePlacePopup);
-formElement.addEventListener('submit', formSubmitHandler); //слушатель для сохрания формы.
-
-
-const buttons = document.querySelectorAll('.element__like');
-
-buttons.forEach ((button) =>{
-  button.addEventListener('click', function(){
-    button.classList.toggle('element__like_active');
-  })
-});
+const placeName = document.querySelector('#place-name');
+const placeLink = document.querySelector('#place-link');
+const formPlace = document.querySelector('#place-form');
 
 // работем по карточкам
 
@@ -115,3 +76,68 @@ initialCards.forEach(function(element){
 
   cardsList.append(cardElement);
 });
+
+//передаем лайки на карточки - старый метод
+// const buttons = document.querySelectorAll('.element__like');
+
+// buttons.forEach ((button) =>{
+//   button.addEventListener('click', function(){
+//     button.classList.toggle('element__like_active');
+//   })
+// });
+
+
+
+//popup user-profile
+function openUserPopup() {
+  popupUser.classList.add('page__popup_visible');
+  currentUserName.value = nameInput.textContent;
+  currentUserJob.value = jobInput.textContent;
+}; //функция открытия попапа
+
+function closeUserPopup() {
+  popupUser.classList.remove('page__popup_visible'); 
+};
+
+function formUserSubmitHandler(evt){
+  evt.preventDefault();
+  nameInput.textContent = currentUserName.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
+  jobInput.textContent = currentUserJob.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
+  closeUserPopup(); //используем уже готовую функцию для закрытия попапа
+} //функция кнопки Сохранить.
+
+
+
+//popup places
+function openPlacePopup() {
+  popupPlace.classList.add('page__popup_visible');
+}
+
+function closePlacePopup() {
+  popupPlace.classList.remove('page__popup_visible');
+}
+
+function addPlace (){
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__image').src= placeLink.value;
+  cardElement.querySelector('.element__title').textContent = placeName.value;
+
+  cardsList.prepend(cardElement);
+}
+
+function formPlaceSubmitHandler(evt) {
+  evt.preventDefault();
+  addPlace();
+  closePlacePopup()
+}
+
+openUserPopupBtn.addEventListener('click', openUserPopup);//слушатель для открытия попапа для редактирования профиля пользователя
+closeUserPopupBtn.addEventListener('click', closeUserPopup);//слушатель для закрытия попапа
+formUser.addEventListener('submit', formUserSubmitHandler); //слушатель для сохрания формы.
+
+openPlacePopupBtn.addEventListener('click', openPlacePopup);
+closePlacePopupBtn.addEventListener('click', closePlacePopup);
+formPlace.addEventListener('submit', formPlaceSubmitHandler);
+
+
