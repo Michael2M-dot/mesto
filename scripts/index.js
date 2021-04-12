bc281dc3ac75615b51de9ea3bbee2955649ae14e
Michael2M <darak.ltd@yandex.ru>
2021-04-12 22:23:59 +0800

    added close windows by Esc button, finished validation

--- scripts/index.js
+++ scripts/index.js
@@ -22,42 +22,31 @@ email: darak.ltd@yandex.ru
 // при вводе в сроке имени, меняем имя вывода на странице
 // при вовде текста в строке должности, менем текст на странице 
 
-// const formUser = document.querySelector('.form__user');
+//переменые для использвоания в скрипте
 const formUser = document.forms.userProfileForm;//форма для редактирования данных пользоватля
 const nameInput = document.querySelector('.profile__user-name');
 const jobInput = document.querySelector('.profile__user-job');
 const userNameInput = formUser.elements.userNameInput;//переменная поля ввода имени для формы редактирования профиля пользователя
 const userJobInput = formUser.elements.userJobInput;//переменная поля ввода професси для формы редактирования профиля пользователя
-// const currentUserName = document.querySelector('.from__user-name'); //получаем и записываем значение переменной из поля по id user-name
-// const currentUserJob = document.querySelector('.form__user-job'); //получаем и записываем значение переменной из поля по id user-job
+// const errorMessages = document.querySelectorAll('.form__input-error')
 const popupUser = document.querySelector('.popup__edit-profile');
 const popupPlace = document.querySelector('.popup__add-place');
-
 const openUserPopupBtn = document.querySelector('.profile__button-edit');
 const closeUserPopupBtn = document.querySelector('#close-userPopup');
 const openPlacePopupBtn = document.querySelector('.profile__button-add');
 const userFormSubmitButton = document.querySelector('#user-submit');
 const closePlacePopupBtn = document.querySelector('#close-placePopup');
-// const formPlace = document.querySelector('.form__place');
 const formPlace = document.forms.placeCardForm;//форма для добавления карточки
-// const placeName = document.querySelector('.form__place-name');
 const placeName = formPlace.elements.placeNameInput;//поле формы добавления карточки, нзвание места
-// const placeLink = document.querySelector('.form__place-link');
 const placeLink = formPlace.elements.placeLinkInput;//поле формы карточки, ссылка на фотографию места
-// const placeFromSubmitButton = formPlace.querySelector('#place-submit');
 const formInputErrors = document.querySelectorAll('.form__input-error');
-
 const cardList = document.querySelector('.elements__list');// место куда добавляем карточку
 const cardTemplate = document.querySelector('.element__template').content; //достаем шаблон из template
 const closePreviewPicturePopupBtn = document.querySelector('#close-PicturePopup');
 const popupPicturePreview = document.querySelector('.popup__picture');
-
-const currentPicture = document.querySelector('.popup__image');
-const currentTitle = document.querySelector('.popup__caption');
-// const placeName = document.querySelector('.form__place-name');
-// const placeLink = document.querySelector('.form__place-link');
+const currentPicture = popupPicturePreview.querySelector('.popup__image');
+const currentTitle = popupPicturePreview.querySelector('.popup__caption');
 const popupWindows = document.querySelectorAll('.popup');//универсальная переменная всех поапов на старнице
-
 
 
 // работем по карточкам
@@ -93,9 +82,10 @@ cardList.addEventListener('click', function (evt, item) {
     eventTarget.closest('.elements__list-item').remove();
   }
   // if (eventTarget.classList.contains('element__image')) {
-  //   handlePreviewPicture(evt, item);
-  // } - дописать слушатель для делегирования по картинке
+  //   handlePreviewPicture(evt.target, item);
+  // } //дописать слушатель для делегирования по картинке
 })
+//TODO = дописать слушатель для делегирования по всплытия для открытия ппопапа просмотра картинки.
 
 //функция добавления карточки на страницу
 function renderCard(item, isPrepend) {
@@ -108,12 +98,46 @@ initialCards.forEach(function (item) {
   renderCard(item);
 });//проходим по массиву и создаем карточки
 
+
 //сабмит для добавления карточек пользователя
 function handleFormPlaceSubmit(evt) {
   evt.preventDefault();
   renderUserCard();
   closePopup(popupPlace);
+  handleDisableButton();//отключаем кнопку сабмита
 };
+
+//функция кнопки Сохранить информацию о пользователе
+function handleFormUserSubmit(evt) {
+  evt.preventDefault();
+  nameInput.textContent = userNameInput.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
+  jobInput.textContent = userJobInput.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
+  closePopup(popupUser); //используем уже готовую функцию для закрытия попапа
+  handleDisableButton();//отключаем кнопку сабмита
+}
+
+function handlePreviewPicture(item) {
+  openPopup(popupPicturePreview);
+  currentPicture.src = item.link;
+  currentTitle.textContent = item.name;
+  currentPicture.alt = item.name;
+}
+
+//функция для обработчика закрытия попапов для всех кнопок закрытия и для все поапов
+function handleCloseWindow(popup, evt) {
+  if (evt.target.classList.contains('page__popup') ||
+      evt.target.classList.contains('popup__button-close') ||
+      evt.key === 'Escape') {
+    console.log('я здесь!')
+    closePopup(popup);
+    handleDisableButton();
+    //обнуляем поля ошибки при закрытии формы через вызов универсально функции hideInputError
+    const inputElements = document.querySelectorAll('.form__fieldset')
+    inputElements.forEach((input) =>
+        hideInputError(input, selectors));
+  }
+};
+
 
 //добавляем событие like - перенесена в универсальное делегирование и всплытие
 // function handleLikeElement(evt) {
@@ -126,26 +150,19 @@ function handleFormPlaceSubmit(evt) {
 // };
 
 //функция добавляет в превью фото картинки и название в попап просмотра изображения
-function handlePreviewPicture(item) {
-  openPopup(popupPicturePreview);
-  currentPicture.src = item.link;
-  currentTitle.textContent = item.name;
-  currentPicture.alt = item.name;
-}
 
 //универсальная функция закрытия попапа
 function closePopup(popup) {
-  popup.classList.remove('page__popup_visible')
+  popup.classList.remove('page__popup_visible');
 }
 
-//функция для обработчика закрытия попапов для всех кнопок закрытия и для все поапов
-function handleCloseWindow(popup, evt) {
-  const eventTarget = evt.target;
-  // const escTarget = evt.keyDown;
-  if (eventTarget.classList.contains('page__popup') ||
-    eventTarget.classList.contains('popup__button-close')) {
-    closePopup(popup);
-  }
+//функция для отключения кнопки submit. преводит кнопку в disabled  и убирает класс, делающий кнопу активной
+function handleDisableButton() {
+  const submitButtons = document.querySelectorAll('.form__submit-btn')
+  submitButtons.forEach((buttonElement) => {
+    buttonElement.setAttribute('disabled', true,);
+    buttonElement.classList.add(selectors.disabledBtnSelector);
+  });
 };
 
 //универсальная функция открытия попапа
@@ -160,13 +177,36 @@ function openUserPopup() {
   userJobInput.value = jobInput.textContent;
 };
 
-//функция кнопки Сохранить информацию о пользователе
-function handleFormUserSubmit(evt) {
-  evt.preventDefault();
-  nameInput.textContent = userNameInput.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
-  jobInput.textContent = userJobInput.value; //присваиваем новые значения с помощью textContent, значения полность перезаписываются
-  closePopup(popupUser); //используем уже готовую функцию для закрытия попапа
+
+//универсальная функция которая запускает все закрытия попапов
+popupWindows.forEach((popup) => {
+  popup.addEventListener('click', evt => handleCloseWindow(popup, evt))
+  document.addEventListener('keydown', evt => handleCloseWindow(popup, evt))
+});
+
+//заведение новой карточки места
+//добавлем карточки от пользователя.
+function renderUserCard() {
+  const item = {
+    name: placeName.value,
+    link: placeLink.value
+  };
+  renderCard(item, true);
 }
+
+//открытие popup places с обнулением полей
+function openUserCardPopup() {
+  openPopup(popupPlace);
+  formPlace.reset();
+  }
+
+//слушатели для попапа редактирования данных пользователя
+openUserPopupBtn.addEventListener('click', openUserPopup);//слушатель для открытия попапа для редактирования профиля пользователя
+formUser.addEventListener('submit', handleFormUserSubmit); //слушатель для сохранеия формы.
+
+//слушатели для попапа добавления карточек
+openPlacePopupBtn.addEventListener('click', openUserCardPopup);
+formPlace.addEventListener('submit', handleFormPlaceSubmit);
 
 //универсальная функция проверки состояния валидности формы для активации кнопки сохранить
 // function setSubmitButtonState(submitButton, isFormValid){
@@ -191,41 +231,6 @@ function handleFormUserSubmit(evt) {
 //   const isValid = placeName.value.length > 1 && placeName.value.length < 30 && placeLink.value.length > 2;
 //   setSubmitButtonState(placeFromSubmitButton, isValid);
 // });
-
-
-//универсальная функция которая запускает все закрытия попапов
-popupWindows.forEach((popup) => {
-  popup.addEventListener('click', e => (handleCloseWindow(popup, e)))
-});
-
-
-//заведение новой карточки места
-//добавлем карточки от пользователя.
-function renderUserCard() {
-  const item = {
-    name: placeName.value,
-    link: placeLink.value
-  };
-  renderCard(item, true);
-}
-
-//открытие popup places с обнулением полей
-function openUserCardPopup() {
-  openPopup(popupPlace);
-
-  formPlace.reset();
-
-}
-
-
-//слушатели для попапа редактирования данных пользователя
-openUserPopupBtn.addEventListener('click', openUserPopup);//слушатель для открытия попапа для редактирования профиля пользователя
-formUser.addEventListener('submit', handleFormUserSubmit); //слушатель для сохранеия формы.
-
-//слушатели для попапа добавления карточек
-openPlacePopupBtn.addEventListener('click', openUserCardPopup);
-formPlace.addEventListener('submit', handleFormPlaceSubmit);
-
 
 
 
