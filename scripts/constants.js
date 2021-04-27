fa89ea94f9159799184e2e8a728ed2e01d580c5d
Michael2M <darak.ltd@yandex.ru>
2021-04-27 11:11:32 +0800

    refactored Prettier

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,22 +5,22 @@ export const selectors = {
   disabledBtnSelector: "form__submit-btn_disabled",
   errorsSelector: ".form__input-error",
   formSection: ".form__fieldset",
-  inputErrorSelector: "form__input-error_active",
+  inputErrorSelector: "form__input-error_active"
 };
 
-//переменые для использвоания в скрипте
+//переменные для использования в скрипте
 const formUser = document.forms.userProfileForm; //форма для редактирования данных пользователя
 const nameInput = document.querySelector(".profile__user-name");
 const jobInput = document.querySelector(".profile__user-job");
 const userNameInput = formUser.elements.userNameInput; //переменная поля ввода имени для формы редактирования профиля пользователя
-const userJobInput = formUser.elements.userJobInput; //переменная поля ввода професси для формы редактирования профиля пользователя
+const userJobInput = formUser.elements.userJobInput; //переменная поля ввода профессии для формы редактирования профиля пользователя
 const openUserPopupBtn = document.querySelector(".profile__button-edit");
 const closeUserPopupBtn = document.querySelector("#close-userPopup");
 const openPlacePopupBtn = document.querySelector(".profile__button-add");
 const popupUser = document.querySelector("#edit-profile");
 const popupPlace = document.querySelector("#add-place");
 const closePlacePopupBtn = document.querySelector("#close-placePopup");
-const popupWindows = document.querySelectorAll(".popup"); //универсальная переменная всех поапов на старнице
+const popupWindows = document.querySelectorAll(".popup"); //универсальная переменная всех попапов на странице
 const cardList = document.querySelector(".elements__list"); // место куда добавляем карточку
 const popupPicturePreview = document.querySelector("#picture-popup");
 const currentPicture = popupPicturePreview.querySelector(".popup__image");
@@ -29,9 +29,10 @@ const closePreviewPicturePopupBtn = document.querySelector(
   "#close-PicturePopup"
 );
 const formPlace = document.forms.placeCardForm; //форма для добавления карточки
-const placeName = formPlace.elements.placeNameInput; //поле формы добавления карточки, нзвание места
-const placeLink = formPlace.elements.placeLinkInput; //поле формы карточки, ссылка на фотографию местад
+const placeName = formPlace.elements.placeNameInput; //поле формы добавления карточки, название места
+const placeLink = formPlace.elements.placeLinkInput; //поле формы карточки, ссылка на фотографию места
 const ESC = "Escape";
+
 
 export {
   formUser,
