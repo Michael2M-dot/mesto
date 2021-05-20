e8a7366b103a7101090786d7913a818c0ed963fc
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-05-20 22:39:47 +0800

    added API class and his methods

--- src/scripts/components/httpClient.js
+++ src/scripts/components/httpClient.js
@@ -17,32 +17,106 @@ const userName = document.querySelector(".profile__user-name");
 const userJob = document.querySelector(".profile__user-job");
 const userAvatar = document.querySelector(".profile__user-avatar");
 
-function getUsedData() {
-  return fetch("https://nomoreparties.co/v1/cohort-24/users/me", {
-    method: "GET",
-    headers: {
-      authorization: "99295e52-decf-4a30-8030-f17c65fb60b0",
-    },
-  });
-}
+export default class Api {
+  constructor(serverUrl, headers) {
+    this._serverUrl = serverUrl;
+    this._headers = headers;
+  }
 
-getUsedData()
-  .then((res) => {
+  _checkStatus(res) {
     if (res.ok) {
       return res.json();
     }
-    return Promise.reject(res.status);
-  })
-  .then((data) => {
-    console.log(data);
-    renderUserData(data);
-  })
-  .catch((err) => {
-    console.log("Error", err);
-  });
+    return Promise.reject(`Ошибка: ${res.status} ${res.statusText}`);
+  }
 
-function renderUserData(data) {
-  userName.textContent = data.name;
-  userJob.textContent = data.about;
-  userAvatar.style.backgroundImage = data.avatar;
+  getUserData() {
+    return fetch(`${this._serverUrl}/user/me`, {
+      headers: this._headers,
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  getInitialCards() {
+    return fetch(`${this._serverUrl}/cards`, {
+      headers: this._headers,
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  updateUserData() {
+    return fetch(`${this._serverUrl}/user/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  addCard() {
+    return fetch(`${this._serverUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  deleteCard(id) {
+    return fetch(`${this._serverUrl}/cards/${id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  likeCard(method, id) {
+    return fetch(`${this._serverUrl}/cards/likes/${id}`, {
+      method: method,
+      headers: this._headers,
+    })
+      .then((res) => this._checkStatus(res))
+  }
+
+  updateAvatar(){
+    return fetch(`${this._serverUrl}/user/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => this._checkStatus(res))
+  }
 }
+
+
+
+
+// function getUsedData() {
+//   return fetch("https://nomoreparties.co/v1/cohort-24/users/me", {
+//     method: "GET",
+//     headers: {
+//       authorization: "99295e52-decf-4a30-8030-f17c65fb60b0",
+//     },
+//   });
+// }
+//
+// getUsedData()
+//   .then((res) => {
+//     if (res.ok) {
+//       return res.json();
+//     }
+//     return Promise.reject(res.status);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     renderUserData(data);
+//   })
+//   .catch((err) => {
+//     console.log("Error", err);
+//   });
+//
+// function renderUserData(data) {
+//   userName.textContent = data.name;
+//   userJob.textContent = data.about;
+//   userAvatar.style.backgroundImage = data.avatar;
+// }
